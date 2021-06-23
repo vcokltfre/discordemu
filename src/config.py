adcb3ce1beff5a0f2f44600a5d8a4ceeb72be052
vcokltfre <vcokltfre@gmail.com>
2021-06-23 01:26:28 +0100

    feat: add config

---
+++ src/config.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from typing import List
+
+from yaml import safe_load
+
+
+with open("./config.yml") as f:
+    config = safe_load(f)
+
+
+@dataclass
+class ConfigDatabase:
+    host: str = "127.0.0.1"
+    port: int = 5432
+    password: str = "password"
+    user: str = "root"
+
+
+@dataclass
+class ConfigUser:
+    id: int
+    token: str
+    username: str
+    discriminator: int
+
+
+@dataclass
+class Config:
+    database: ConfigDatabase
+    applications: List[ConfigUser]
+
+
+cfg_db = ConfigDatabase(**config["database"])
+cfg_apps = [ConfigUser(**app) for app in config["applications"]]
+
+CONFIG = Config(cfg_db, cfg_apps)
