f419476507c11ca197016551d28f4e716a0ad651
vcokltfre <vcokltfre@gmail.com>
2021-06-23 01:46:22 +0100

    chore: add gateway routes

--- src/api/routers/__init__.py
+++ src/api/routers/__init__.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter
 
+from .gateway import router as gateway_router
+
 
 router = APIRouter(prefix="/api")
+
+router.include_router(gateway_router)
---
+++ src/api/routers/gateway.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+
+from src.config import CONFIG
+from src.models.api.gateway import GetGateway, GetGatewayBot, SessionStartLimit
+
+
+router = APIRouter(prefix="/gateway")
+
+@router.get("")
+async def get_gateway() -> GetGateway:
+    return GetGateway(url=f"ws://{CONFIG.host}")
+
+
+@router.get("/bot")
+async def get_gateway_bot() -> GetGatewayBot:
+    session_start_limit = SessionStartLimit(
+        total=1000,
+        remaining=999,
+        reset_after=14400000,
+        max_concurrency=1,
+    )
+
+    return GetGatewayBot(
+        url="ws://{CONFIG.host}",
+        shards=1,  # TODO: Some sort of sharding logic
+        session_start_limit=session_start_limit,
+    )
